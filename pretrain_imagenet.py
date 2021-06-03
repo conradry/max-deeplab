658d5fad67b1b4fb4f8444b30db444ee4fe0ec36
clarencechen <clarencechenct@gmail.com>
2021-06-02 21:19:49 -0700

    add tpu compat code

--- pretrain_imagenet.py
+++ pretrain_imagenet.py
@@ -33,6 +33,8 @@ parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
+parser.add_argument('-t', '--tpu', type=bool, action='store_true',
+                    help='use TPU accelerator')
 parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
